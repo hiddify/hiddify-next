98283213b65eed8380e116b1a276376f082e4f8b
problematicconsumer <problematicconsumer@protonmail.com>
2023-07-18 12:57:31 +0330

    Fix code gen bug

--- lib/core/core_providers.dart
+++ lib/core/core_providers.dart
@@ -1,8 +1,6 @@
 import 'package:hiddify/core/locale/locale.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'core_providers.g.dart';
-
-@Riverpod(keepAlive: true)
-TranslationsEn translations(TranslationsRef ref) =>
-    ref.watch(localeControllerProvider).translations();
+final translationsProvider = Provider<TranslationsEn>(
+  (ref) => ref.watch(localeControllerProvider).translations(),
+);
