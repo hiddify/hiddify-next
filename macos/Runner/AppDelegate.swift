e8184a1e8eb8be4ef5dcb8a91ee9b4229a334de4
problematicconsumer <problematicconsumer@protonmail.com>
2023-08-25 13:29:55 +0330

    Fix macos silent start

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -4,6 +4,7 @@ import FlutterMacOS
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
+    // https://github.com/leanflutter/window_manager/issues/214
+    return false
   }
 }
