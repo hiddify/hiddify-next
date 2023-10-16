7b2129ad6dde49925445b3bd0ca64c1323196afa
GFWFighter <gfwfightingsoldier@gmail.com>
2023-10-16 20:43:42 +0330

    Initialize Packet Tunnel + Config

---
+++ ios/SingBoxPacketTunnel/PacketTunnelProvider.swift
@@ -0,0 +1,36 @@
+//
+//  PacketTunnelProvider.swift
+//  SingBoxPacketTunnel
+//
+//  Created by GFWFighter on 7/24/1402 AP.
+//
+
+import NetworkExtension
+
+class PacketTunnelProvider: NEPacketTunnelProvider {
+
+    override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
+        // Add code here to start the process of connecting the tunnel.
+    }
+    
+    override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
+        // Add code here to start the process of stopping the tunnel.
+        completionHandler()
+    }
+    
+    override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)?) {
+        // Add code here to handle the message.
+        if let handler = completionHandler {
+            handler(messageData)
+        }
+    }
+    
+    override func sleep(completionHandler: @escaping () -> Void) {
+        // Add code here to get ready to sleep.
+        completionHandler()
+    }
+    
+    override func wake() {
+        // Add code here to wake up.
+    }
+}
