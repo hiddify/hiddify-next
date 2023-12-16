4345b97d728c4b29a6fbdf7e0e5e904bd23de407
problematicconsumer <problematicconsumer@protonmail.com>
2023-12-16 18:16:46 +0330

    Fix macOS restore from dock

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -7,4 +7,18 @@ class AppDelegate: FlutterAppDelegate {
     // https://github.com/leanflutter/window_manager/issues/214
     return false
   }
+
+  // window manager restore from dock: https://leanflutter.dev/blog/click-dock-icon-to-restore-after-closing-the-window
+  override func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+      if !flag {
+          for window in NSApp.windows {
+              if !window.isVisible {
+                  window.setIsVisible(true)
+              }
+              window.makeKeyAndOrderFront(self)
+              NSApp.activate(ignoringOtherApps: true)
+          }
+      }
+      return true
+  }
 }
