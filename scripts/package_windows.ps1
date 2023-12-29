87a0fce83575eff9eacca69b22dbc7387ecace53
problematicconsumer <problematicconsumer@protonmail.com>
2023-12-29 11:45:34 +0330

    Fix windows portable again

--- scripts/package_windows.ps1
+++ scripts/package_windows.ps1
@@ -1,10 +1,11 @@
-New-Item -ItemType Directory -Force -Name "dist/tmp"
+New-Item -ItemType Directory -Force -Name "dist\tmp"
 New-Item -ItemType Directory -Force -Name "out"
 
 # windows setup
-Get-ChildItem -Recurse -File -Path "dist" -Filter "*windows-setup.exe" | Copy-Item -Destination "dist/tmp/hiddify-next-setup.exe" -ErrorAction SilentlyContinue
-Compress-Archive -Force -Path "dist/tmp/hiddify-next-setup.exe",".github/help/mac-windows/*.url" -DestinationPath "out/hiddify-windows-x64-setup.zip"
+Get-ChildItem -Recurse -File -Path "dist" -Filter "*windows-setup.exe" | Copy-Item -Destination "dist\tmp\hiddify-next-setup.exe" -ErrorAction SilentlyContinue
+Compress-Archive -Force -Path "dist\tmp\hiddify-next-setup.exe",".github\help\mac-windows\*.url" -DestinationPath "out\hiddify-windows-x64-setup.zip"
 
 # windows portable
-Copy-Item -Force -Path "build/windows/x64/runner/Release/*",".github/help/mac-windows/*.url" -Destination "dist/tmp/hiddify-next"
-Compress-Archive -Force -Path "dist/tmp/hiddify-next" -DestinationPath "out/hiddify-windows-x64-portable.zip"
+robocopy "build\windows\x64\runner\Release" "dist\tmp\hiddify-next\" /e
+robocopy ".github\help\mac-windows" "dist\tmp\hiddify-next" "*.url"
+Compress-Archive -Force -Path "dist\tmp\hiddify-next" -DestinationPath "out\hiddify-windows-x64-portable.zip"
