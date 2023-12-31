d8ffeb2ef48ce2b6c43c8caa5cc3d065f98cd9e8
problematicconsumer <problematicconsumer@protonmail.com>
2023-12-31 10:32:05 +0330

    Remove desktop auto connect

--- lib/features/connection/widget/connection_wrapper.dart
+++ lib/features/connection/widget/connection_wrapper.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hiddify/core/preferences/service_preferences.dart';
 import 'package:hiddify/features/connection/notifier/connection_notifier.dart';
 import 'package:hiddify/utils/custom_loggers.dart';
-import 'package:hiddify/utils/platform_utils.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ConnectionWrapper extends StatefulHookConsumerWidget {
@@ -27,13 +25,15 @@ class _ConnectionWrapperState extends ConsumerState<ConnectionWrapper>
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 2)).then(
-      (_) async {
-        if (ref.read(startedByUserProvider) && PlatformUtils.isDesktop) {
-          loggy.debug("previously started by user, trying to connect");
-          return ref.read(connectionNotifierProvider.notifier).mayConnect();
-        }
-      },
-    );
+    // remove for now...
+    //
+    // Future.delayed(const Duration(seconds: 2)).then(
+    //   (_) async {
+    //     if (ref.read(startedByUserProvider) && PlatformUtils.isDesktop) {
+    //       loggy.debug("previously started by user, trying to connect");
+    //       return ref.read(connectionNotifierProvider.notifier).mayConnect();
+    //     }
+    //   },
+    // );
   }
 }
