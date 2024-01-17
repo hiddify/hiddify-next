fd34690d49d4d6e2cfc9d91923c671107e34cf60
Amir Mohammadi <amirkamangar@icloud.com>
2024-01-17 15:43:38 +0330

    switch to local spm

---
+++ ios/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+     name: "Libcore",
+     platforms: [
+        // Minimum platform version
+         .iOS(.v13),
+         .tvOS(.v13)
+     ],
+     products: [
+         .library(
+             name: "Libcore",
+             targets: ["Libcore"]),
+     ],
+     dependencies: [
+         // No dependencies
+     ],
+     targets: [
+        .binaryTarget(
+            name: "Libcore",
+            path: "Frameworks/Libcore.xcframework"
+        )
+     ]
+ )
