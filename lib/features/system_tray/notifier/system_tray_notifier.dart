65142c1efa425be3c956ae580438528fddcdb6aa
sky96111 <sky96111@outlook.com>
2024-03-29 17:17:21 +0800

    fix: make dark tray icon windows-only

--- lib/features/system_tray/notifier/system_tray_notifier.dart
+++ lib/features/system_tray/notifier/system_tray_notifier.dart
@@ -125,14 +125,16 @@ class SystemTrayNotifier extends _$SystemTrayNotifier with AppLogger {
   }
 
   static String get _trayIconPath {
-    final Brightness brightness = WidgetsBinding.instance.platformDispatcher.platformBrightness;
-    bool isDarkMode = brightness == Brightness.dark;
-    if (isDarkMode) {
-      if (Platform.isWindows) return Assets.images.trayIconIco;
-      return Assets.images.trayIconPng.path;
-    } else {
-      if (Platform.isWindows) return Assets.images.trayIconDarkIco;
-      return Assets.images.trayIconDarkPng.path;
+    if (Platform.isWindows) {
+      final Brightness brightness = WidgetsBinding.instance.platformDispatcher.platformBrightness;
+      bool isDarkMode = brightness == Brightness.dark;
+      if (isDarkMode) {
+        return Assets.images.trayIconIco;
+      } else {
+        return Assets.images.trayIconDarkIco;
+      }
     }
+    
+    return Assets.images.trayIconPng.path;
   }
 }
