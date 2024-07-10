3c02c8d72b28bbeb8d8b9bc88750d71de87677a7
Shane N. Woolum <132649516+donnanszex7654@users.noreply.github.com>
2024-07-10 10:09:57 +0000

    fix bugg

--- lib/features/config_option/data/config_option_repository.dart
+++ lib/features/config_option/data/config_option_repository.dart
@@ -32,12 +32,12 @@ abstract class ConfigOptions {
     mapTo: (value) => value.name,
   );
 
-  static final CloudflareST = PreferencesNotifier.create<bool, bool>(
+  static final cloudflareST = PreferencesNotifier.create<bool, bool>(
     "cloudflare-st",
     false,
   );
 
-  static final CloudflareNumber = PreferencesNotifier.create<int, int>(
+  static final cloudflareNumber = PreferencesNotifier.create<int, int>(
     "cloudflare-number",
     200,
     validator: (value) => value > 10,
@@ -327,8 +327,8 @@ abstract class ConfigOptions {
 
   static final Map<String, StateNotifierProvider<PreferencesNotifier, dynamic>> preferences = {
     "region": region,
-    "cloudflare-st": CloudflareST,
-    "cloudflare-number": CloudflareNumber,
+    "cloudflare-st": cloudflareST,
+    "cloudflare-number": cloudflareNumber,
     "block-ads": blockAds,
     "service-mode": serviceMode,
     "log-level": logLevel,
@@ -423,8 +423,8 @@ abstract class ConfigOptions {
 
       return SingboxConfigOption(
         region: ref.watch(region).name,
-        cloudflareST: ref.watch(CloudflareST),
-        cloudflareNumber: ref.watch(CloudflareNumber),
+        cloudflareST: ref.watch(cloudflareST),
+        cloudflareNumber: ref.watch(cloudflareNumber),
         blockAds: ref.watch(blockAds),
         executeConfigAsIs: false,
         logLevel: ref.watch(logLevel),
