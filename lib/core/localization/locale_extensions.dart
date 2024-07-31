c7d2fc462646c6c5fba13dd5f0bb1b5ae48ec919
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-07-31 22:44:48 +0200

    fix; font

--- lib/core/localization/locale_extensions.dart
+++ lib/core/localization/locale_extensions.dart
@@ -4,7 +4,7 @@ import 'package:hiddify/gen/fonts.gen.dart';
 import 'package:hiddify/gen/translations.g.dart';
 
 extension AppLocaleX on AppLocale {
-  String get preferredFontFamily => this == AppLocale.fa ? FontFamily.shabnam : (Platform.isIOS || Platform.isMacOS ? "" : FontFamily.emoji);
+  String get preferredFontFamily => this == AppLocale.fa ? FontFamily.shabnam : (!Platform.isWindows ? "" : FontFamily.emoji);
 
   String get localeName => switch (flutterLocale.toString()) {
         "en" => "English",
