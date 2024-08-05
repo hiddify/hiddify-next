607f1297f8c6c3930da878db8f2fc66a08fda14d
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-08-05 15:48:20 +0200

    check macos

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ class AppDelegate: FlutterAppDelegate {
     // https://github.com/leanflutter/window_manager/issues/214
     return false
   }
-  func applicationDidFinishLaunching(_ aNotification: Notification) {
+  override func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Request notification authorization
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge]) { granted, error in
             if let error = error {
